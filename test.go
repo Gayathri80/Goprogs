ce0d1bcb077f5af755b648bbdf09d741c42a4ba3
Gayathri <gayathriv.qa@gmail.com>
2021-07-30 13:15:26 -0400

    init

---
+++ test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//var str = "14"
+	// var input
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	fmt.Printf("\ntype of value %T", input)
+	// convert string to integer
+	input = strings.TrimSpace(input)
+	myVar, error := strconv.Atoi(input)
+	fmt.Printf("\ntype of value %T", myVar)
+	fmt.Println("\nerror:", error)
+	fmt.Println("number:", myVar)
+}
